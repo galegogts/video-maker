5680b25c3eeb18ef0abb25aec3c2bfbcf244fce7
GTSGalego <torres.guilherme@live.com>
2021-03-10 11:18:56 -0300

    Adicionar robo de Texto, procurando no wikipedia, limpando o texto e separando por sentenças

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const readline = require('readline-sync');
+const robots = {
+    text: require('./robots/text.js')
+}
 
-function Start(){
+async function Start(){
     const content ={};
+    content.searchLang = askAndReturnLang();
     content.searchTerm = askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix();
+    content.searchInput = {
+        "articleName": content.searchTerm,
+        "lang": content.searchLang
+      };
+
+
+    await robots.text(content);
 
     function askAndReturnSearchTerm(){
         return readline.question('Type a Wikipedia search Term: ');
@@ -14,6 +25,13 @@ function Start(){
         const selectedPrefixText = prefixes[selectedPrefixIndex];
 
         return selectedPrefixText;
+    }
+    function askAndReturnLang(){
+        const langs = ['en','pt'];
+        const selectedLangsIndex = readline.keyInSelect(langs, 'Choose one Option: ');
+        const selectedLangText = langs[selectedLangsIndex];
+
+        return selectedLangText;
     }
     console.log(content)
 }
---
+++ robots/text.js
@@ -0,0 +1,51 @@
+const algorithmia = require('algorithmia');
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
+const sentenceBoundaryDetection = require('sbd');
+
+async function robot(content){
+    await fetchContentFromWikipedia(content);
+    sanitizedContent(content);
+    breakContentIntoSentences(content);   
+    
+    async function fetchContentFromWikipedia(content){
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
+        const WikipediaAlgorithmia = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
+        const wikipediaResponse = await WikipediaAlgorithmia.pipe(content.searchInput);
+        const wikipediaContent = wikipediaResponse.get();
+
+        content.sourceContentOriginal = wikipediaContent.content;
+    }
+
+    function sanitizedContent(content){
+        const withoutBlankLinesAndMarkdown = removeBlankAndMarkdown(content.sourceContentOriginal);
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown);
+
+        content.sourceContentSanitized = withoutDatesInParentheses;
+
+        function removeBlankAndMarkdown(text){
+            const allLines = text.split('\n');
+            const withoutBlankAndMarkdown = allLines.filter((line => {
+                if(line.trim().lenght === 0 || line.trim().startsWith('=')){
+                    return false
+                }
+                return true;
+            }))
+            return withoutBlankAndMarkdown.join(' ');
+        }
+    }
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ');
+    }
+    function breakContentIntoSentences(content){
+        content.sentences = [];
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
+        sentences.forEach((sentences) => {
+            content.sentences.push({
+                text: sentences,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+}
+module.exports = robot;
