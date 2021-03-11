4e2f2c6a20301a250d2e60da1e806eead9e199ab
GTSGalego <torres.guilherme@live.com>
2021-03-11 15:14:18 -0300

    Adicionar Google Images e API do Google

---
+++ robots/image.js
@@ -0,0 +1,39 @@
+const google = require('googleapis').google;
+const customSearch = google.customsearch('v1');
+const state = require('./state.js');
+
+const googleSearchCredentials = require("../credentials/google-search.json");
+async function robot(){
+    const content = state.load();
+    const numResultados = 2;
+
+    await fetchImagesOfAllSentences(content);
+
+    state.save(content);
+    
+    
+    async function fetchImagesOfAllSentences(content){
+        for (const sentence of content.sentences){
+            const query = `${content.searchTerm} ${sentence.keywords[0]}`;
+            sentence.images = await fetchGoogleAndReturnImagesLinks(query);
+            sentence.googleSearchQuery = query;
+        }
+    }
+
+    async function fetchGoogleAndReturnImagesLinks(query){
+        const reponse = await customSearch.cse.list({
+            auth: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: 'image',
+            num:numResultados
+        })
+    
+        const imageUrl = reponse.data.items.map((item) => {
+            return item.link;
+        })
+        return imageUrl;
+    }
+
+}
+module.exports = robot;
