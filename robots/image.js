46c80f667e462a55c23816f7258ab392ae12c603
GTSGalego <torres.guilherme@live.com>
2021-03-13 13:22:41 -0300

    baixar imagens do google image

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -5,9 +6,10 @@ const state = require('./state.js');
 const googleSearchCredentials = require("../credentials/google-search.json");
 async function robot(){
     const content = state.load();
-    const numResultados = 2;
+    const numResultados = 3;
 
     await fetchImagesOfAllSentences(content);
+    await downloadAllImages(content);
 
     state.save(content);
     
@@ -33,6 +35,35 @@ async function robot(){
             return item.link;
         })
         return imageUrl;
+    }
+
+    async function downloadAllImages(content){
+        content.downloadedImages = [];
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            const images = content.sentences[sentenceIndex].images;
+
+            for(let imageIndex = 0; imageIndex < images.length; imageIndex ++){
+                const imageUrl  = images[imageIndex];
+
+                try{
+                    if(content.downloadedImages.includes(imageUrl)){
+                        throw new Error('Imagem já baixada');
+                    }
+                    await downloadAndSave(imageUrl,`${sentenceIndex}-original.png`);
+                    content.downloadedImages.push(imageUrl);
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Imagem Baixada: ${imageUrl}`);
+                    break;
+                }catch(error){
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar: (${imageUrl}): ${error}`);
+                }
+            }
+        }
+    }
+    async function downloadAndSave(url, fileName){
+        return imageDownloader.image({
+            url: url,
+            dest: `./content/${fileName}`
+        })
     }
 
 }
